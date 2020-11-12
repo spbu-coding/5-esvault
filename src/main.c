94fe341ef42cf68acf8bc94298b5000ddefee480
Иван Азарников <azarnikov01@gmail.com>
2020-11-13 00:04:15 +0300

    main.c moved to src/

---
+++ src/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void input() {
+    char buffer[5];
+    gets(buffer);
+    fflush(stdout);
+}
+
+void other() {
+    printf("**************Secret function**************\n");
+    fflush(stdout);
+    exit(0);
+}
+
+int main() {
+    printf("%p\n", &other);
+    input();
+    printf("Normal out\n");
+    return 0;
+}
